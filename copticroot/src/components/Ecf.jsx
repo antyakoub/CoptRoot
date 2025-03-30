15de3852bd7f7f84fcc45649c9a971454dfb8b7b
tony <antyakoub@gmail.com>
2025-03-29 20:00:54 -0400

    feat: add Early Church Fathers page

---
+++ copticroot/src/components/Ecf.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Ecf.css';
+
+const TextComponent = () => {
+  return (
+    <div className="text-container">
+      <div className="left-side">
+        <p className="left-text">This is the text aligned to the left.</p>
+      </div>
+      <div className="right-side">
+        <p className="right-text">
+          This is the paragraph that will be aligned to the right third of the screen. 
+          Add more content here to see the effect. It will only take up the right third of the screen.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TextComponent;
+
+
+// The writings of the Early Church Fathers are considered crucial for understanding Christian doctrine, theology, and tradition. These writings are viewed as authoritative, though they are not equal to Scripture, but are considered a vital part of the Church's spiritual and theological heritage.
