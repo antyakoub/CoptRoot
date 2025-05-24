f1fc245b31885e670faea36d855c1b5d0f2a8265
tony <antyakoub@gmail.com>
2025-05-24 11:45:27 -0400

    chore: move images and json to public, replace js data with json

--- copticroot/src/components/Podcasts.jsx
+++ copticroot/src/components/Podcasts.jsx
@@ -1,29 +1,33 @@
-// PodcastGrid.jsx
-
-import React from "react";
-import podcastData from "../assets/podcasts/podcasts";
+import React, { useEffect, useState } from "react";
 import "../styles/Podcasts.css";
 
-const PodcastBox = ({ title, host, thumbnail, link }) => {
-  return (
-    <div className="music-box">
-      <div className="music-content">
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          <img src={thumbnail} alt={title} className="music-image" />
-        </a>
-      </div>
-      <div className="music-info">
-        <h2>{title}</h2>
-        <h3>{host}</h3>
-      </div>
+const PodcastBox = ({ title, host, thumbnail, link }) => (
+  <div className="music-box">
+    <div className="music-content">
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img src={thumbnail} alt={title} className="music-image" />
+      </a>
     </div>
-  );
-};
+    <div className="music-info">
+      <h2>{title}</h2>
+      <h3>{host}</h3>
+    </div>
+  </div>
+);
 
 const PodcastGrid = () => {
+  const [podcasts, setPodcasts] = useState([]);
+
+  useEffect(() => {
+    fetch("/assets/podcastAssets/podcasts.json")
+      .then((res) => res.json())
+      .then(setPodcasts)
+      .catch(console.error);
+  }, []);
+
   return (
     <div className="podcast-grid">
-      {podcastData.map((pod, index) => (
+      {podcasts.map((pod, index) => (
         <PodcastBox
           key={index}
           title={pod.title}
